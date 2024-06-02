4f1c0bc02f83df1844d33586332ece234191feb5
Nguyen Phuong Nha <nphuonha2101@gmail.com>
2024-06-02 22:12:34 +0700

    #6 - feat: create WaveBackground component in Home

---
+++ src/components/pages/WaveBackground.tsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import "./css//WaveBackground.css";
+
+const WaveBackground = () => {
+    return (
+        <Container className="position-absolute top-0 start-0" fluid>
+            <div className="container-fluid wave-container" id="wave-container-up"></div>
+            <div className="container-fluid wave-container" id="wave-container-bottom"></div>
+        </Container>
+    );
+
+}
+
+export default WaveBackground;
