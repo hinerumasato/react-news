5306aac0ceec1b8ee1ab721ec4068bf3b0f76d49
Nguyen Phuong Nha <nphuonha2101@gmail.com>
2024-06-09 23:38:05 +0700

    16 - refactor: create PaginatePage and add it to Home

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,13 +1,23 @@
 import { Container } from "react-bootstrap"
 import WaveBackground from "./WaveBackground"
 import SubscribeNews from "@/components/partials/SubscribeNews"
+import PaginatePage from "@/components/partials/PaginatePage"
+import { useState } from "react"
 
 export const Home = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+        console.log(`Current page: ${page}`);
+    }
+
     return (
         <Container>
             <WaveBackground />
             {/* <SliderContainer /> */}
             <SubscribeNews />
+            <PaginatePage currentPage={currentPage} totalPages={10} onPageChange={handlePageChange} />
         </Container>
     )
 }
