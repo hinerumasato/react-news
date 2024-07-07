9e912132e1240abce4eba865066321388fcbdfd9
PhuongCute <bichphuonglaithi06032003@gmail.com>
2024-07-07 18:48:11 +0700

    #40 - feat:Initialize search states

--- src/components/pages/Search/SearchNews.tsx
+++ src/components/pages/Search/SearchNews.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {SearchNewsProps} from "@/interfaces/SearchNewsProps.ts";
 import {Button, Form, Modal} from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {rssCategories} from "@/constants";
+import INewsItem from "@/interfaces/INewsItem.ts";
+import {useFeeds} from "@/hooks/useFeeds.ts";
 
 export const SearchNews:React.FC<SearchNewsProps> =({showModal, setShowModal}) => {
+    const [inputValue, setInputValue] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('Tất cả');
+    const [searchResult, setSearchResult] = useState<INewsItem[]>([]);
+    const navigate = useNavigate();
+    const handleCategoryChange = (event:{target: {value: React.SetStateAction<string>;};}) => {
+        setSelectedCategory(event.target.value);
+    }
+    const handleChangeInpput = (event: {target: {value: React.SetStateAction<string>;};}) => {
+        setInputValue(event.target.value);
+    }
+
     const handleClose = () => setShowModal(false);
     return(
         <Modal show={showModal} onHide={handleClose}>
@@ -28,7 +42,9 @@ export const SearchNews:React.FC<SearchNewsProps> =({showModal, setShowModal}) =
                         <Form.Label className="me-3">
                             Danh mục thể loại
                         </Form.Label>
-                        <Form.Select className="w-50 rounded-2 py-2 px-1"></Form.Select>
+                        <Form.Select className="w-50 rounded-2 py-2 px-1">
+
+                        </Form.Select>
                     </div>
                 </Container>
             </Modal.Body>
