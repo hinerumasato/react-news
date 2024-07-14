7bd61ad85caf776e930f33186ca7a6032664b6b1
PhuongCute <bichphuonglaithi06032003@gmail.com>
2024-07-14 20:39:42 +0700

    #49 - feat: finish page About

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -6,10 +6,10 @@ import "@/assets/css/About.scss";
 
 export const About: React.FC = () => {
     return (
-        <Container className="py-5">
-            <Card className="card-one text-center mb-5 z-3">
+        <Container className="p-4">
+            <Card className="card-one text-center mb-5 z-3 p-3">
                 <Card.Body>
-                    <Card.Title className="fs-2">Giới thiệu</Card.Title>
+                    <Card.Title className="fs-2 mb-5">Giới thiệu</Card.Title>
                     <Card.Text className="fs-4 fw-light">
                         Chào mừng bạn đến với trang tin tức của chúng tôi. Đây là nguồn tin tức đáng tin cậy, cung cấp
                         thông tin nhanh chóng và chính xác về các sự kiện diễn ra trên khắp Việt Nam và thế giới.
@@ -23,47 +23,44 @@ export const About: React.FC = () => {
                 </Card.Body>
             </Card>
 
-            <h2 className=" text-center">Thông tin thành viên</h2>
+            <h2 className=" text-center mt-5">Thông tin thành viên</h2>
             <Row className="my-5">
-                <Col md={4}>
+                <Col md={4} className="p-3">
                     <Card className="z-3 rounded-3 card-one">
-                        <img src="src/components/pages/About/img11.webp"/>
+                        <img src="src/assets/css/image/img11.webp"/>
                         <Card.Body>
                             <Card.Title className="text-center">TRẦN THẮNG LỢI</Card.Title>
                             <Card.Text>
-                                Member A is responsible for the editorial content. With years of experience in
-                                journalism, A ensures that our news is accurate and timely.
+                                Với khả năng code siêu pro anh luôn sẵn sàng hỗ trợ đồng đội và giải quyết mọi vấn đề phát sinh. Sự nhiệt huyết và tinh thần trách nhiệm của anh là nguồn động lực lớn cho cả team.
                             </Card.Text>
                         </Card.Body>
                     </Card>
                 </Col>
-                <Col md={4}>
+                <Col md={4} className="p-3">
                     <Card className="z-3 card-one">
-                        <img src="src/components/pages/About/img22.jpg"/>
+                        <img src="src/assets/css/image/img22.jpg"/>
                         <Card.Body>
                             <Card.Title className="text-center">NGUYỄN PHƯƠNG NHÃ</Card.Title>
                             <Card.Text>
-                                Member B handles the technical side of our website. B is a skilled developer who ensures
-                                our site runs smoothly and efficiently.
+                                Xây dựng nền tảng vững chắc cho mọi hoạt động diễn ra, với những kiến thức chuyên sâu về hệ thống, anh đảm bảo mọi thứ vận hành một cách trơn tru nhất có thể.
                             </Card.Text>
                         </Card.Body>
                     </Card>
                 </Col>
-                <Col md={4}>
+                <Col md={4} className="p-3">
                     <Card className="z-3 card-one">
-                        <img src="src/components/pages/About/img33.jpg"/>
+                        <img src="src/assets/css/image/img33.jpg"/>
                         <Card.Body>
                             <Card.Title className="text-center">LẠI THỊ BÍCH PHƯỢNG</Card.Title>
                             <Card.Text>
-                                Member C is in charge of marketing and outreach. C's expertise helps us reach a wider
-                                audience and engage with our readers.
+                                Với niềm đam mê về thiết kế và sự tỉ mỉ đến từng chi tiết, cô luôn đảm bảo rằng độc giả sẽ có những phút giây đọc tin tức thoải mái và thú vị nhất.
                             </Card.Text>
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
             <WaveBackground/>
-            <Row>
+            <Row className="p-3">
                 <Col md={7} className="p-4">
                     <h2>Liên hệ với chúng tôi </h2>
                     <p className="fs-5 my-4">
@@ -75,7 +72,7 @@ export const About: React.FC = () => {
                     </form>
                 </Col>
                 <Col md={5}>
-                    <img src="src/components/pages/About/img1.webp" className="img-fluid"/>
+                    <img src="src/assets/css/image/img1.webp" className="img-fluid rounded-3"/>
                 </Col>
             </Row>
         </Container>
