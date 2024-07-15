f295919cdc3ce7bc2ea50476783fb0bd03fdfa8a
陳勝利  （ひねる まさと） <thangloitran406@gmail.com>
2024-07-15 15:18:39 +0700

    feat: add share feature

--- src/components/pages/NewsDetail/NewsDetail.tsx
+++ src/components/pages/NewsDetail/NewsDetail.tsx
@@ -1,11 +1,14 @@
 import { Application } from "@/constants/application";
 import { useCrawlData, useParseFeed } from "@/hooks";
+import { useFeeds } from "@/hooks/useFeeds";
+import INewsItem from "@/interfaces/INewsItem";
 import { Urls } from "@/utils";
-import { LegacyRef, useEffect, useRef } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { LegacyRef, useEffect, useRef, useState } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
 import './NewsDetail.scss';
 import { Social } from "./Social/Social";
+import { Waves } from "@/components/vendors";
 
 const lazyLoading = () => {
     const images = document.querySelectorAll('.content img');
@@ -39,6 +42,8 @@ export const NewsDetail = () => {
     const textType = Urls.toCategoryType(type);
     const socialRef = useRef<HTMLElement | null>(null);
     const articleRef = useRef<HTMLElement>();
+    const [relatedFeeds, setRelatedFeeds] = useState<Array<INewsItem>>();
+    const feeds = useFeeds(`${type}.rss`, 6);
 
     useEffect(() => {
         if (feedDetail?.title)
@@ -47,28 +52,56 @@ export const NewsDetail = () => {
             lazyLoading();
     }, [feedDetail?.content, feedDetail?.title])
 
+    useEffect(() => {
+        feeds.then(data => setRelatedFeeds(data));
+    }, [feeds]);
+
     return (
-        <article ref={articleRef as LegacyRef<HTMLElement>} id="newsDetail">
-            <Container style={{ width: '900px', maxWidth: '100%' }}>
-                <Row>
-                    <Col md={2} className="mt-5">
-                        <Social url={url} ref={socialRef} />
-                    </Col>
-                    <Col md={10}>
-                        {textType && <p className="text-secondary pt-3">{textType}</p>}
-                        <h1 className="fw-bold">{feedDetail?.title}</h1>
-                        <div className="d-flex align-items-center gap-2">
-                            <img width={30} height={30} className="rounded rounded-circle" src={feedDetail?.authorAvatar} alt={feedDetail?.authorName} />
-                            <div className="d-md-flex align-items-center gap-3">
-                                <p className="m-0 fw-bold">{feedDetail?.authorName}</p>
-                                <p className="m-0 text-secondary">{feedDetail?.authorTime}</p>
+        <section id="feedDetails">
+            <article ref={articleRef as LegacyRef<HTMLElement>} id="newsDetail">
+                <Container style={{ width: '900px', maxWidth: '100%' }}>
+                    <Row>
+                        <Col md={2} className="mt-5">
+                            <Social url={url} ref={socialRef} />
+                        </Col>
+                        <Col md={10}>
+                            {textType && <p className="text-secondary pt-3">{textType}</p>}
+                            <h1 className="fw-bold">{feedDetail?.title}</h1>
+                            <div className="d-flex align-items-center gap-2">
+                                <img width={30} height={30} className="rounded rounded-circle" src={feedDetail?.authorAvatar} alt={feedDetail?.authorName} />
+                                <div className="d-md-flex align-items-center gap-3">
+                                    <p className="m-0 fw-bold">{feedDetail?.authorName}</p>
+                                    <p className="m-0 text-secondary">{feedDetail?.authorTime}</p>
+                                </div>
                             </div>
-                        </div>
-                        <p className="py-3" style={{ fontStyle: 'italic' }}>{feedDetail?.sapo}</p>
-                        <div className="content" dangerouslySetInnerHTML={{ __html: feedDetail?.content as string }}></div>
-                    </Col>
-                </Row>
-            </Container>
-        </article>
+                            <p className="py-3" style={{ fontStyle: 'italic' }}>{feedDetail?.sapo}</p>
+                            <div className="content" dangerouslySetInnerHTML={{ __html: feedDetail?.content as string }}></div>
+                            <div className="related-news">
+                                <h2 className="fw-bold d-flex align-items-center">
+                                    <Waves />
+                                    Bài viết liên quan
+                                </h2>
+                                <Row className="row-cols-md-3">
+                                    {relatedFeeds && relatedFeeds.map((item, index) => (
+                                        <Col key={index} className="mt-3">
+                                            <Card>
+                                                <Card.Img variant="top" src={item.newsImg} />
+                                                <Card.Body>
+                                                    <Card.Title className="related-news-title">{item.title}</Card.Title>
+                                                    <Card.Text className="related-news-snippet">
+                                                        {item.contentSnippet}
+                                                    </Card.Text>
+                                                    <Link to={Urls.toNewsDetailLink(item.link) as string} className="button btn-custom">Đọc thêm</Link>
+                                                </Card.Body>
+                                            </Card>
+                                        </Col>
+                                    ))}
+                                </Row>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </article>
+        </section>
     )
 }
