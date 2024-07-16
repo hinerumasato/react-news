5c8cb9c42fd47e8d897e8cb1e4571734ad798683
PhuongCute <bichphuonglaithi06032003@gmail.com>
2024-07-16 21:41:12 +0700

    #61 refactor: edit interface category bar

--- src/components/pages/News/CategoryBar.tsx
+++ src/components/pages/News/CategoryBar.tsx
@@ -1,41 +1,39 @@
 import "@/assets/css/CategoryBar.scss";
 import { CategoryBarButton } from "@/components/vendors/Buttons/CategoryBarButton";
 import { rssCategories } from "@/constants";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 type category = { ['name']: string, ['rss']: string }
 
 export const CategoryBar = (
     { onChangeCategory }: { onChangeCategory: (category: category) => void }
 ) => {
-
     const initialRssLink = localStorage.getItem('currentRssLink') || rssCategories[0].rss;
     const [currentRssLink, setCurrentRssLink] = useState(initialRssLink);
-
     useEffect(() => {
         localStorage.setItem('currentRssLink', currentRssLink);
     }, [currentRssLink]);
-
     function changeCategory(category: { ['name']: string, ['rss']: string }) {
         onChangeCategory(category)
         setCurrentRssLink(category.rss)
     }
-
     return (
-        <div className="category-bar mt-5">
-            {rssCategories.map((category, index) => {
-                return (
-
-                    <CategoryBarButton key={index} data-link={category.rss}
-                        active={category.rss === currentRssLink}
-                        onClick={() => {
-                            changeCategory(category)
-                        }}>
-                        {category.name}
-                    </CategoryBarButton>
-                )
-            })}
+        <div className="navbar-container" >
+            <div className="navbar-wrapper">
+                <div className="navbar category-bar">
+                    {rssCategories.map((category, index) => {
+                        return (
+                            <CategoryBarButton key={index} data-link={category.rss}
+                                               active={category.rss === currentRssLink}
+                                               onClick={() => {
+                                                   changeCategory(category)
+                                               }}>
+                                {category.name}
+                            </CategoryBarButton>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
-
